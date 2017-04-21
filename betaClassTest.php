8d9d53abb85838464941a8d6e0b5bad53affcdf4
belou <belou@localhost.localdomain>
2017-04-21 16:49:58 +0200

    Remove some lines

--- betaClassTest.php
+++ betaClassTest.php
@@ -36,10 +36,6 @@ class My_BetaClassTest extends PHPUnit_Framework_TestCase
      */
     public function testAdd()
     {
-        // Remove the following lines when you implement this test.
-        //$this->markTestIncomplete(
-        //  'This test has not been implemented yet.'
-        //);
     }
 }
 ?>
