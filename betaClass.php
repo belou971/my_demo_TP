739305a21b35a0d45cd586ca1980974c505da9a1
belou <belou@localhost.localdomain>
2017-04-21 16:45:13 +0200

    add a comment

--- betaClass.php
+++ betaClass.php
@@ -1,4 +1,10 @@
 <?php
+/***********************************
+ *  
+ *   Cette classe retourne la somme
+ *   de deux valeurs.
+ *
+ ***********************************/
 class My_BetaClass {
  public function add($a, $b) {
     return $a + $b;
